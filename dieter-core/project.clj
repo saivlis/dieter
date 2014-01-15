eb49da5824a10d0cf70967852674f31aef4b0e1f
silvia <silvia.schreier@innoq.com>
2014-01-15 11:23:04 +0100

    removing v8 for windows support

--- dieter-core/project.clj
+++ dieter-core/project.clj
@@ -7,8 +7,6 @@
   :dependencies [[ring/ring-core "1.1.8"]
                  [clj-time "0.4.4"]
                  [com.google.javascript/closure-compiler "r1592"]
-                 [clj-v8 "0.1.4"]
-                 [clj-v8-native "0.1.4"]
                  [org.mozilla/rhino "1.7R4"]
                  [org.clojure/clojure "1.4.0"]]
   :dev-dependencies [[org.clojure/clojure "1.4.0"]]
